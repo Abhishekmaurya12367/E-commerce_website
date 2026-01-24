66f740948395ab9f00e403d3729e0ea7c38d37ac
abhishek maurya <abhishekrmaurya2020@gmail.com>
2026-01-25 00:52:41 +0530

    Initial project commit

--- greatkart/category/models.py
+++ greatkart/category/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here. 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -10,4 +10,5 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
-      
+    def get_url(self):
+        return reverse('product_by_category', args=[self.slug])
--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 
-def store(request,category_slug=None):
-    categories=None
-    product=None
-    if category_slug!=None: 
-        categories=get_object_or_404(Category,slug=category_slug)
-        product=Product.objects.filter(category=categories,is_available=True)
-        product_count=product.count()
-    else:
-     products = Product.objects.filter(is_available=True)
-     context = {
+def store(request, category_slug=None):
+    category = None
+    products = Product.objects.filter(is_available=True)
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category, is_available=True)
+
+    product_count = products.count()
+
+    context = {
         'products': products,
-     }
+        'product_count': product_count,
+        'category': category,
+    }
+
     return render(request, 'store/store.html', context)
