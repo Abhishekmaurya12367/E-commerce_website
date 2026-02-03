a0f05695c860782841451f2c4741f0a02aaff29a
abhishek maurya <abhishekrmaurya2020@gmail.com>
2026-02-04 00:24:23 +0530

    first commit

--- greatkart/store/models.py
+++ greatkart/store/models.py
@@ -42,6 +42,7 @@ class Variation(models.Model):
     variation_value = models.CharField(max_length=500)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
+    object=VariationManager()
 
     def __unicode__(self):
         return self.product
