9cbae9abebd08897d93d39a340be04a1596ac395
abhishek maurya <abhishekrmaurya2020@gmail.com>
2026-02-05 00:30:07 +0530

    first commit

---
+++ greatkart/account/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Accounts
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
+
+    class Meta:  
+        model = Accounts
+        fields = [
+            'first_name',
+            'last_name',
+            'phone_number',
+            'email',
+            'password'
+        ]
